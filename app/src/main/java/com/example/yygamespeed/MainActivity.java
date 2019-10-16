60b2963ea7d5371037567a5c5388748c713234a0
zq134 <zq13408401721@139.com>
2019-10-16 14:57:36 +0800

    设置文本内容

--- app/src/main/java/com/example/yygamespeed/MainActivity.java
+++ app/src/main/java/com/example/yygamespeed/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txt = findViewById(R.id.txt);
+        txt.setText("添加分子");
 
     }
 }
